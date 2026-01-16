3e83c80c6e5eb9e83181ab33ab32e72cb3eecac3
Gourav <gourav@turka.tech>
2026-01-16 17:58:26 +0530

    completed

--- list.py
+++ list.py
@@ -1,5 +1,10 @@
-l = []
+# l = []
 
-for num in range(1, 100):
-    l.append(num)
-print(l)
+# for num in range(1, 100):
+#     l.append(num)
+# print(l)
+
+l = [num for num in range(1, 100)]
+evenNumbers = [num for num in l if num % 2 == 0]
+print(evenNumbers[-2])
+
